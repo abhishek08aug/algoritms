1a49152e2495e00001ad7bd5c675ad9717499f42
Abhishek Srivastava <abhishek08aug@gmail.com>
2020-09-23 01:47:19 +0530

    added tabular method for 0-1 knapsack

--- src/com/example/dp/ZeroOneKnapsack.java
+++ src/com/example/dp/ZeroOneKnapsack.java
@@ -7,15 +7,17 @@ public class ZeroOneKnapsack {
         int [] weight = new int [] {13, 2, 8, 4, 9};
         int knapsackCapacity = 12;
 
-        System.out.println(getMaximumProfitRecursive(value, weight, value.length, knapsackCapacity));
+        System.out.println("Max profit (Recursive): " + getMaximumProfitRecursive(value, weight, value.length, knapsackCapacity));
+        System.out.println("Max profit (Memoized): " + getMaximumProfitMemoized(value, weight, value.length, knapsackCapacity));
+        System.out.println("Max profit (Tabular): " + getMaximumProfitTabular(value, weight, value.length, knapsackCapacity));
 
-        int [][] table = new int[value.length+1][knapsackCapacity+1];
-        for(int i=0; i<value.length+1; i++) {
-            for(int j=0; j<knapsackCapacity+1; j++) {
-                table[i][j] = -1;
-            }
-        }
-        System.out.println(getMaximumProfitMemoized(value, weight, value.length, knapsackCapacity, table));
+        value = new int [] { 60, 100, 120 };
+        weight = new int [] { 10, 20, 30 };
+        knapsackCapacity = 50;
+
+        System.out.println("Max profit (Recursive): " + getMaximumProfitRecursive(value, weight, value.length, knapsackCapacity));
+        System.out.println("Max profit (Memoized): " + getMaximumProfitMemoized(value, weight, value.length, knapsackCapacity));
+        System.out.println("Max profit (Tabular): " + getMaximumProfitTabular(value, weight, value.length, knapsackCapacity));
     }
 
     public static int getMaximumProfitRecursive(int [] value, int [] weight, int n, int knapsackCapacity) {
@@ -30,11 +32,21 @@ public class ZeroOneKnapsack {
         return Math.max(value[n-1] + getMaximumProfitRecursive(value, weight, n-1, knapsackCapacity-weight[n-1]), getMaximumProfitRecursive(value, weight, n-1, knapsackCapacity));
     }
 
-    public static int getMaximumProfitMemoized(int [] value, int [] weight, int n, int knapsackCapacity, int [][] table) {
+    public static int getMaximumProfitMemoized(int [] value, int [] weight, int n, int knapsackCapacity) {
         if(knapsackCapacity == 0 || n == 0) {
             return 0;
         }
 
+        int [][] table = new int[n+1][knapsackCapacity+1];
+        for(int i=0; i<n+1; i++) {
+            for(int j=0; j<knapsackCapacity+1; j++) {
+                table[i][j] = -1;
+            }
+        }
+        return _getMaximumProfitMemoized(value, weight, n, knapsackCapacity, table);
+    }
+
+    private static int _getMaximumProfitMemoized(int [] value, int [] weight, int n, int knapsackCapacity, int [][] table) {
         if(table[n][knapsackCapacity] != -1) {
             return table[n][knapsackCapacity];
         }
@@ -43,8 +55,31 @@ public class ZeroOneKnapsack {
             table[n][knapsackCapacity] = getMaximumProfitRecursive(value, weight, n-1, knapsackCapacity);
             return table[n][knapsackCapacity];
         }
-
         table[n][knapsackCapacity] = Math.max(value[n-1] + getMaximumProfitRecursive(value, weight, n-1, knapsackCapacity-weight[n-1]), getMaximumProfitRecursive(value, weight, n-1, knapsackCapacity));
+        return table[n][knapsackCapacity];
+    }
+
+    public static int getMaximumProfitTabular(int [] value, int [] weight, int n, int knapsackCapacity) {
+        if(knapsackCapacity == 0 || n == 0) {
+            return 0;
+        }
+
+        int [][] table = new int[n+1][knapsackCapacity+1];
+        for(int i=0; i<=n; i++) {
+            table[i][0] = 0;
+        }
+        for(int j=0; j<=knapsackCapacity; j++) {
+            table[0][j] = 0;
+        }
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=knapsackCapacity; j++) {
+                if(weight[i-1] > j) {
+                    table[i][j] = table[i-1][j];
+                } else {
+                    table[i][j] = Math.max(value[i-1] + table[i-1][j-weight[i-1]], table[i-1][j]);
+                }
+            }
+        }
         return table[n][knapsackCapacity];
     }
 }
